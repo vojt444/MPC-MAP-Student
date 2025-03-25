3f2c5481152ace82de2f426cb05269d89e450c73
Vojtech Piroch <vojta.piroch@gmail.com>
2025-03-25 19:59:13 +0100

    little improvements

--- algorithms/path_planning/plan_path.m
+++ algorithms/path_planning/plan_path.m
@@ -17,18 +17,18 @@ else
     % line05 = [1,5;1,6];
     % line06 = [1,6;1,7];
     % line07 = [1,7;1,7.5];
-    % line0 = [1,1;1,7.5];
-    % arc0 = generate_arc_waypoints(2,7.5,1,90,90,'CW',10);
-    % line1 = [2,8.5;3.5,8.5];
-    % arc1 = generate_arc_waypoints(3.5, 7, 1.5, 0, 90, 'CW', 15);
-    % line2 = [5,7;5,4];
-    % arc2 = generate_arc_waypoints(7, 4, 2, 180, 180, 'CCW', 20);
-    % line3 = [9,4;9,9];
+    line0 = [1,1;1,7.5];
+    arc0 = generate_arc_waypoints(2,7.5,1,90,90,'CW',10);
+    line1 = [2,8.5;3.5,8.5];
+    arc1 = generate_arc_waypoints(3.5, 7, 1.5, 0, 90, 'CW', 15);
+    line2 = [5,7;5,4];
+    arc2 = generate_arc_waypoints(7, 4, 2, 180, 180, 'CCW', 20);
+    line3 = [9,4;9,9];
     % public_vars.path = [line01; line02; line03; line04; line05; line06; line07; arc0; line1; arc1; line2; arc2; line3];
-    % public_vars.path = [line0; arc0; line1; arc1; line2; arc2; line3];
-    arc0 = generate_arc_waypoints(9,2,7,0,180,'CW',50);
+    public_vars.path = [line0; arc0; line1; arc1; line2; arc2; line3];
+    % arc0 = generate_arc_waypoints(9,2,7,0,180,'CW',50);
 
-    public_vars.path = [arc0];
+    % public_vars.path = [arc0];
     path = public_vars.path;
 
 end
