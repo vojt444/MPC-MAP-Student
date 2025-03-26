3ee0d603b6ead467a8d2a54ec2ced8c94cf87db4
Vojtech Piroch <vojta.piroch@gmail.com>
2025-03-26 21:49:30 +0100

    planning improved

--- algorithms/path_planning/astar.m
+++ algorithms/path_planning/astar.m
@@ -5,51 +5,39 @@ extended_map = extend_map(read_only_vars.discrete_map.map, public_vars.walls_wid
 [rows, cols] = size(extended_map);
 
 robot_est_pos = [public_vars.estimated_pose(1) public_vars.estimated_pose(2)];
-robot_disc_x_pos = round(((robot_est_pos(1) - read_only_vars.map.limits(1))/(read_only_vars.map.limits(3) - read_only_vars.map.limits(1)))*read_only_vars.discrete_map.dims(1));
-robot_disc_y_pos = round(((robot_est_pos(2) - read_only_vars.map.limits(2))/(read_only_vars.map.limits(4) - read_only_vars.map.limits(2)))*read_only_vars.discrete_map.dims(2));
+robot_disc_row = round(((robot_est_pos(1) - read_only_vars.map.limits(1))/(read_only_vars.map.limits(3) - read_only_vars.map.limits(1)))*read_only_vars.discrete_map.dims(2));
+robot_disc_col = round(((robot_est_pos(2) - read_only_vars.map.limits(2))/(read_only_vars.map.limits(4) - read_only_vars.map.limits(2)))*read_only_vars.discrete_map.dims(1));
 
-robot_discrete_pos = [robot_disc_x_pos robot_disc_y_pos];
+start = [robot_disc_row robot_disc_col];
+goal = [read_only_vars.discrete_map.goal(2) read_only_vars.discrete_map.goal(1)];
+
+if extended_map(start(1), start(2)) == 1 || extended_map(goal(1), goal(2)) == 1
+    path = [];
+    return;
+end
 
 open_set = false(rows, cols);
 closed_set = false(rows, cols);
-
 g_cost = inf(rows, cols);
 f_cost = inf(rows, cols);
-
 parent = zeros(rows, cols, 2);
 
-sx = robot_discrete_pos(1);
-sy = robot_discrete_pos(2);
 
-if extended_map(sx, sy) == 1
-    path = [];
-    return;
-end
-
-gx = read_only_vars.discrete_map.goal(1);
-gy = read_only_vars.discrete_map.goal(2);
-
-if extended_map(gx, gy) == 1
-    path = [];
-    return;
-end
-
-g_cost(sx, sy) = 0;
-f_cost(sx, sy) = heuristic([sx sy], [gx gy]);
-open_set(sx, sy) = true;
+g_cost(start(1), start(2)) = 0;
+f_cost(start(1), start(2)) = heuristic(start, goal);
+open_set(start(1), start(2)) = true;
 
 directions = [-1 -1;-1 0;-1 1;0 -1;0 1;1 -1;1 0;1 1];
 
 while any(open_set(:))
-    % [min_val min_idx] = min(f_cost(:).*open_set(:) + inf*(~open_set(:)));
     masked_f_cost = f_cost;
     masked_f_cost(~open_set) = inf;
-    [min_val, min_idx] = min(masked_f_cost(:));
+    [~, min_idx] = min(masked_f_cost(:));
 
-    [cx cy] = ind2sub(size(f_cost), min_idx);
+    [cx, cy] = ind2sub(size(f_cost), min_idx);
 
-    if cx == gx && cy == gy
-        path = reconstruct_path(parent, read_only_vars.discrete_map.goal);
+    if cx == goal(1) && cy == goal(2)
+        path = reconstruct_path(parent, goal);
         return;
     end
 
@@ -73,7 +61,7 @@ while any(open_set(:))
         if ~open_set(nx, ny) || t_g_cost < g_cost(nx, ny)
             parent(nx, ny, :) = [cx cy];
             g_cost(nx, ny) = t_g_cost;
-            f_cost(nx, ny) = t_g_cost + heuristic([nx ny], [gx gy]);
+            f_cost(nx, ny) = t_g_cost + heuristic([nx ny], goal);
             open_set(nx, ny) = true;
         end
     end
